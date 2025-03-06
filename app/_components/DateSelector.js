a9be93ff574dfad23bd188c0e1a69d0da072efcb
oo_ng <oolagoke@umich.edu>
2025-03-06 08:54:21 -0500

    finished setting up creating reservation functionaloity

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,6 +1,6 @@
 
 "use client"
-import { isWithinInterval } from "date-fns";
+import { differenceInDays, isPast, isSameDay, isWithinInterval } from "date-fns";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 import { useReservation } from "../_context/ReservationContext";
@@ -17,16 +17,19 @@ function isAlreadyBooked(range, datesArr) {
 
 function DateSelector({settings, cabin, bookedDates}) {
     const {range, setRange, resetRange} = useReservation()
+
+    const displayRange = isAlreadyBooked(range, bookedDates)?{}:range;
     
 //   const {id, name,regularPrice, discount, numNights } = cabin
-  const regularPrice = 23;
-  const discount = 23;
-  const numNights = 23;
-  const cabinPrice = 23;
+  const {regularPrice, discount} = cabin;
+  const numNights = differenceInDays(displayRange.to, displayRange.from);
+  
+  const cabinPrice = numNights * (regularPrice - discount);
 //   const range = { from: null, to: null };
 
   // SETTINGS
   const { minBookingLength, maxBookingLength } = settings;
+  console.log("bookedDates", bookedDates)
 
   return (
     <div className="flex flex-col justify-between">
@@ -37,7 +40,7 @@ function DateSelector({settings, cabin, bookedDates}) {
       }}
         className="pt-12 place-self-center"
         mode="range"
-        selected={range}
+        selected={displayRange}
         min={minBookingLength + 1}
         max={maxBookingLength}
         fromMonth={new Date()}
@@ -45,6 +48,7 @@ function DateSelector({settings, cabin, bookedDates}) {
         toYear={new Date().getFullYear() + 5}
         captionLayout="dropdown"
         numberOfMonths={2}
+        disabled = {(curDate)=>isPast(curDate) || bookedDates.some((date=> isSameDay(date, curDate)))}
       />
 
       <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
